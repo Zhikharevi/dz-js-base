68dd739b95a6fd9441a08107d006cd6ea8e0f37a
Zhikharevi <65420470+Zhikharevi@users.noreply.github.com>
2024-11-20 23:51:56 +0300

    Delete 4-navigator directory

--- 4-navigator/script.js
+++
@@ -1,6 +0,0 @@
-const positionLatitude = 5;
-const positionLongitude = 7; 
-const destinationLatitude = 10;
-const destinationLongitude = 15;
-const distance = ((positionLatitude - destinationLatitude) ** 2 + (positionLongitude - destinationLongitude) ** 2) ** (1/2)
-console.log(`Дистанция ${distance}`)
